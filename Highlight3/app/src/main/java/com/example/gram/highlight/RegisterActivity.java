1b60a698966ab7e6f90ae4375aff4fae9b3b5cd5
leemin00 <dlalsgur6869@gmail.com>
2018-07-25 13:05:26 +0900

    HighLight_login_ver

---
+++ Highlight3/app/src/main/java/com/example/gram/highlight/RegisterActivity.java
@@ -0,0 +1,64 @@
+package com.example.gram.highlight;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.gram.highlight.MainManuActivity;
+import com.example.gram.highlight.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText email_join;
+    private EditText pwd_join;
+    private ImageButton sign_btn;
+    FirebaseAuth firebaseAuth;
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        email_join = (EditText) findViewById(R.id.email_join);
+        pwd_join = (EditText) findViewById(R.id.pwd_join);
+        sign_btn = (ImageButton) findViewById(R.id.sign_btn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        sign_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_join.getText().toString().trim();
+                String pwd = pwd_join.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Email로 회원가입 성공", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(RegisterActivity.this, ChooseActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "회원가입 등록 에러", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+}
